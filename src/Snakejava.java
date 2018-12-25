8fdfa9f04720f5345024b4b6ca0357525d7db29e
nguyenthuanbg <Nguyenthuan98bg@gmail.com>
2018-12-25 22:05:29 +0700

    Snake Move

--- src/Snakejava.java
+++ src/Snakejava.java
@@ -1,3 +1,6 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
 import javax.swing.JFrame;
 
 public class Snakejava extends JFrame {
@@ -7,12 +10,47 @@ public class Snakejava extends JFrame {
 	public Snakejava() {
 		setSize(500,500);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setVisible(true);
 		game = new GameScreen();
 		add(game);
+		
+		
+		this.addKeyListener(new handler());
+		setVisible(true);
 	}
 	
 	public static void main(String[] args) {
 		Snakejava f = new Snakejava();
+	}
+	private class handler implements KeyListener{
+
+		@Override
+		public void keyPressed(KeyEvent e) {
+			//System.out.println("key is pressed");
+			if(e.getKeyCode() == KeyEvent.VK_UP) {
+				game.snake.setVecto(Snake.GO_UP);
+			}
+			if(e.getKeyCode() == KeyEvent.VK_DOWN) {
+				game.snake.setVecto(Snake.GO_DOWN);
+			}
+			if(e.getKeyCode() == KeyEvent.VK_LEFT) {
+				game.snake.setVecto(Snake.GO_LEFT);
+			}
+			if(e.getKeyCode() == KeyEvent.VK_RIGHT) {
+				game.snake.setVecto(Snake.GO_RIGHT);
+			}
+		}
+
+		@Override
+		public void keyReleased(KeyEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void keyTyped(KeyEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+		
 	}
 }
